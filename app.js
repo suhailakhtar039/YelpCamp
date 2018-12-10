eab82825192e420309887ee2dc23927a05bb2852
suhailakhtar039 <suhailakhtar039@gmail.com>
2018-12-10 15:28:30 +0530

    add mongolab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes	=require("./routes/comments"),
 	indexRoutes		=require("./routes/index");
 
 // seedDB();//seed the database
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v11",{useNewUrlParser:true});
+// mongoose.connect("mongodb://localhost:27017/yelp_camp_v11",{useNewUrlParser:true});
+mongoose.connect("mongodb://suhailakhtar039:suhail786@ds129914.mlab.com:29914/yelpcamp",{useNewUrlParser:true});
+//mongodb://suhailakhtar39:suhail786@ds129914.mlab.com:29914/yelpcamp
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname+"/public"));
@@ -39,7 +41,7 @@ app.use(function(req,res,next){
     res.locals.success=req.flash("success");
     // res.locals.success=req.flash("success");
 	next();
-})
+});
 app.use("/",indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
@@ -47,4 +49,4 @@ app.use("/campgrounds/:id/comments",commentRoutes);
 
 app.listen(process.env.PORT||3000,function(){
 	console.log("app has started");
-})
+});
